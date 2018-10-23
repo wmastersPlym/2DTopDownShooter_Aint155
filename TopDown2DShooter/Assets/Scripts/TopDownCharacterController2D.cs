b1b44eb147f5d27d2e20be3a0b3346c8b08f29a4
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2018-10-23 16:12:21 +0100

    Finished week 5 tutorials

---
+++ TopDown2DShooter/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(x, y) * speed;
+        rb.angularVelocity = 0.0f;
+    }
+}
